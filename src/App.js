7d31ad213d084a5aff76a9dbf4a5a0c5795b8831
Fintech79 <abdelrahman.faisal@gmail.com>
2018-03-20 23:52:35 -0700

    123

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import heroes from './heroes.json'
+import Wrapper from './components/Wrapper'
+import Title from './components/Title'
+
+
+
+class App extends Component {
+
+  class App extends Component {
+    state = {
+        message: "Click an image to begin!",
+        topScore: 0,
+        curScore: 0,
+        heroes: heroes,
+        unselectedHeroes: heroes
+    }
+
+    componentDidMount() {
+    }
+
+    shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    selecta = type => {
+        const findheroe = this.state.unselectedHeroes.find(item => item.heroe === heroe);
+
+        if(findHeroe === undefined) {
+            // failure to select a new hero
+            this.setState({ 
+                message: "You guessed incorrectly!",
+                topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+                curScore: 0,
+                heroes: heroes,
+                unselectedHeroes: heroes
+            });
+        }
+        else {
+            // success to select a new dog
+            const newHeroes = this.state.unselectedHeroes.filter(item => item.hero !== hero);
+            
+            this.setState({ 
+                message: "You guessed correctly!",
+                curScore: this.state.curScore + 1,
+                heroes: heroes,
+                unselectedHeroes: newHeroes
+            });
+        }
+
+        this.shuffleArray(heroes);
+    };
+  
+    render() {
+      return (
+          <Wrapper>
+              <Navpills
+                  message={this.state.message}
+                  curScore={this.state.curScore}
+                  topScore={this.state.topScore}
+              />
+              <Title />
+              {
+                  this.state.heroes.map(hero => (
+                      <HeroCard
+                          breed={hero.type}
+                          image={hero.image}
+                          selectHero={this.selectHero} 
+                          curScore={this.state.curScore}
+                      />
+                  ))
+              }
+          </Wrapper>
+      );
+  }
+}
+
+export default App;
+
+
+
+
+    
